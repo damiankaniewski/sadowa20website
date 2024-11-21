5e5ff5e0e8b3780bd046cd0a82d10425a8ea396d
Damian <daba9@op.pl>
2024-11-21 21:37:48 +0100

    contact form test

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,6 +5,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -16,7 +17,7 @@ import {
 export class ContactComponent {
   contactForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -27,6 +28,41 @@ export class ContactComponent {
   }
 
   onSubmit() {
-    // do dodania z aws
+    if (this.contactForm.invalid) {
+      alert('Formularz jest niepoprawny. Uzupełnij wszystkie wymagane pola.');
+      return;
+    }
+
+    const formData = this.contactForm.value;
+
+    // Przygotowanie danych do wysyłki
+    const body = {
+      email: 'damian.kaniewski.contact@gmail.com',
+      subject: `Sadowa 20 - Wiadomość od: ${formData.name}`,
+      message: `Imię i nazwisko - ${formData.name}\nEmail - ${formData.email}\nTelefon - ${formData.phone}\n\n${formData.message}`,
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'x-api-key':
+        "pQ5,psD6vUm~&cf2P'tGTo]V`P(E@]bYS[V&8BrVXoS=SqQ(_>G6Vaa~82-ap%e",
+    });
+
+    this.http
+      .post(
+        'https://jc5vg6se5e.execute-api.eu-north-1.amazonaws.com/dev/send-mail',
+        body,
+        { headers }
+      )
+      .subscribe(
+        (response) => {
+          alert('Wiadomość została wysłana pomyślnie!');
+          this.contactForm.reset();
+        },
+        (error) => {
+          console.error('Błąd podczas wysyłania wiadomości:', error);
+          alert('Nie udało się wysłać wiadomości. Spróbuj ponownie.');
+        }
+      );
   }
 }
